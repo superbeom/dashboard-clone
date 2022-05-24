fd81d0e1876265298a865ae6c20b452ad3d848a2
superbeom <it.super.beom@gmail.com>
2022-05-24 11:25:06 +0900

    Update Ecommerce Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ text, size, color, bgColor, borderRadius }) => {
+  return (
+    <button
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      type="button"
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
