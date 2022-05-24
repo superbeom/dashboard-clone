f5e6874d502292b6ef252ec43c78091b7ba8048a
superbeom <it.super.beom@gmail.com>
2022-05-24 12:11:52 +0900

    Update Revenue Section - Add SparkLine

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -61,6 +61,73 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
+
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,438</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$48,438</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+
+              <div className="mt-5">
+                <SparkLine
+                  id="line-sparkline"
+                  type="Line"
+                  width="250px"
+                  height="80px"
+                  color="blue"
+                  currentColor="blue"
+                  data={SparklineAreaData}
+                />
+              </div>
+
+              <div className="mt-10">
+                <Button
+                  text="Download Report"
+                  size="md"
+                  color="white"
+                  bgColor="blue"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
