c9e58a84a0f1a0b77f71464415e4a2395dbe4f3a
superbeom <it.super.beom@gmail.com>
2022-05-25 06:18:59 +0900

    Update Calendar App

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import {
+  ScheduleComponent,
+  Month,
+  Week,
+  Day,
+  WorkWeek,
+  Agenda,
+  Resize,
+  DragAndDrop,
+  Inject,
+} from "@syncfusion/ej2-react-schedule";
+
+import { scheduleData } from "../data/dummy";
+
+import { Header } from "../components";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+
+      <ScheduleComponent
+        height={650}
+        eventSettings={{ dataSource: scheduleData }}
+      >
+        <Inject
+          services={[Month, Week, Day, WorkWeek, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
 };
 
 export default Calendar;
