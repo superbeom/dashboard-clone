d0fe54d4d0219124bd138b6099da910f13291c94
superbeom <it.super.beom@gmail.com>
2022-05-24 15:23:04 +0900

    Update Customers Page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Edit,
+  Sort,
+  Filter,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  const editing = { allowDeleting: true, allowEditing: true };
+  const selectionSettings = { persistSelection: true };
+
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+
+      <GridComponent
+        width="auto"
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={["Delete"]}
+        editSettings={editing}
+        selectionSettings={selectionSettings}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+
+        <Inject services={[Page, Selection, Edit, Sort, Filter, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
