d3b15137942be395c1d4771252092f25bfabf367
superbeom <it.super.beom@gmail.com>
2022-05-25 10:45:55 +0900

    Update Line on Charts

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  LineSeries,
+  DateTime,
+  Legend,
+  Inject,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import { useStateContext } from "../../context/StateContext";
+
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentTheme } = useStateContext();
+
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{
+        enable: true,
+        fill: currentTheme === "Dark" ? "#fff" : "#33373E",
+      }}
+      background={currentTheme === "Dark" ? "#33373E" : "#fff"}
+      legendSettings={{
+        background: currentTheme === "Dark" ? "#fff" : "#33373E",
+        textStyle: {
+          color: currentTheme === "Dark" ? "#33373E" : "#fff",
+        },
+      }}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
